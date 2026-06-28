9bcf1f655062853894e6bae9644feb5f93a409d3
Nikhil Murthy <nikhilmurthy1@live.com>
2026-06-28 19:02:03 -0400

    Update Top10LargestCloudFiles.ps1
    Updated the pathname comment for clarity.

--- Top10LargestCloudFiles.ps1
+++ Top10LargestCloudFiles.ps1
@@ -1,3 +1,3 @@
-$directory = "C:\users\nikhi\OneDrive"
+$directory = "C:\users\nikhi\OneDrive" # Change pathname to root in cloud directory
 $top10LargestFiles = dir -r $directory | Sort-Object Length -Descending | Select-Object -First 10 | Format-Table -Property DirectoryName, Name, Length
 Write-Output($top10LargestFiles)
